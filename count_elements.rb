e0b36182cf2d5386b57eab49a8ac25c02635da8c
Sam Ashtar <samashtarm@gmail.com>
2018-08-28 23:00:51 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
-def count_elements(array)
-  # code goes here
+require 'pry'
+
+ def count_elements (animals)
+   newhash = Hash.new(0)
+   animals.each do |v|
+  newhash[v] += 1
+  newhash.each do |k, v|
+end
+end
+newhash
 end
  
