681c04cda761be89b32831c9bc17a006dfa0103f
Sam Ashtar <samashtarm@gmail.com>
2018-08-28 22:58:31 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,6 +1,13 @@
 require 'pry'
 
  def count_elements (animals)
+   newhash = Hash.new(0)
+   animals.each do |v|
+  b[v] += 1
+  b.each do |k, v|
+  puts "#{k} appears #{v} times"
+end
+end
    
 end
  
